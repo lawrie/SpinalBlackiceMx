6e4eb5160a0f9a4f0ab072d6ca67796b66fa2f15
lawrie <lawrie.griffiths@ntlworld.com>
2019-11-06 17:25:17 +0000

    Added generated verilog

---
+++ BlueLed.v
@@ -0,0 +1,10 @@
+// Generator : SpinalHDL v1.3.5    git head : f0505d24810c8661a24530409359554b7cfa271a
+// Date      : 06/11/2019, 16:39:57
+// Component : BlueLed
+
+
+module BlueLed (
+      output  io_blueLed);
+  assign io_blueLed = (! 1'b1);
+endmodule
+
